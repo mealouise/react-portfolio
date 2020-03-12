aa7848c5e1632fe714fdda821c1ccd4c9a1ebf56
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-12 15:37:48 +0000

    version 1.2.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import dicegame from '../images/dicegame.png';
 import rugbyscoreboard from '../images/rugbyscoreboard.png';
 import cocktail from '../images/cocktail.png';
+import northernfail from '../images/northernfail.png';
+import todolist from '../images/todolist.png';
 
 
 export default function Home(props) {
@@ -11,18 +13,26 @@ export default function Home(props) {
                 <h1 className="home__header">Hi, I'm Emma</h1>
                 <h2 className="home__description">A trainee Junior Javascript Web Developer at Code Nation</h2>
             </div>
-            <div>
+            <div className="home__content">
                 <h3 className="home__portfolio">Portfolio</h3>
-                <ul className="home__projects">
-                    <li>Dice Game JS</li>
-                    <img className="home__dicegame" src={dicegame} alt="dicegame-screenshot" />
-                    <li>Rugby Scoreboard JS</li>
-                    <img className="home__dicegame" src={rugbyscoreboard} alt="rugbyscoreboard-screenshot" />
-                    <li>Weather API Node</li>
-                    <li>Mem project Node</li>
-                    <img className="home__dicegame" src={cocktail} alt="cocktail-screenshot" />
-                </ul>
+                <div className="home__screenshots">
+                    <ul className="home__projects">
+                        <div>
+                            <li>Northern Fail </li>
+                            <img className="home__dicegame" src={northernfail} alt="northernfail-screenshot" />
+                        </div>
+                        <div>
+                            <li>To do List JS</li>
+                            <img className="home__dicegame" src={todolist} alt="todolist-screenshot" />
+                        </div>
+                        <div>
+                            <li>Mem project Node</li>
+                            <img className="home__dicegame" src={cocktail} alt="cocktail-screenshot" />
+                        </div>
+                    </ul>
+                </div>
             </div>
         </div>
+        
     )
 }
